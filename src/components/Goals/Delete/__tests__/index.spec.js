ba0d7872dcd95cd1dc0e331f4e6ef5b03fad52e6
Gerard Burns <gjburns9@hotmail.com>
2019-02-17 21:34:08 +0000

    add delete goal test

---
+++ src/components/Goals/Delete/__tests__/index.spec.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow'
+import { Delete } from '../';
+import { mount } from 'enzyme';
+
+describe('Delete', () => {
+  let testContext = {};
+
+  beforeEach(() => {
+    testContext = {};
+    testContext.auth = { userId: 1, rememberToken: 'rememberToken' }
+    testContext.deleteGoalMock = jest.fn()
+
+    testContext.component = (
+      <Delete
+        auth={testContext.auth}
+        deleteGoal={testContext.deleteGoalMock}
+        goalId={1}
+        loading={false}
+        error={undefined}
+      />
+    );
+  });
+
+  describe('snapshot', () => {
+    beforeEach(() => {
+      const renderer = new ShallowRenderer();
+      testContext.result = renderer.render(testContext.component);
+    });
+
+    it('renders the component correctly', () => {
+      expect(testContext.result).toMatchSnapshot();
+    });
+  });
+
+  describe('deleting', () => {
+    beforeEach(() => {
+      const wrapper = mount(testContext.component);
+      wrapper.simulate('click');
+    });
+
+    it('calls the delete goal mock with the correct arguments', () => {
+      expect(testContext.deleteGoalMock).toBeCalledWith({
+        variables: { goalId: 1, auth: testContext.auth }
+      });
+    });
+  });
+});
