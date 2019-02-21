d67bdf998ae65aba78066f6ecb5cb78e871dc5ee
Gerard Burns <gjburns9@hotmail.com>
2019-02-21 21:30:08 +0000

    animated tickbox

--- src/components/Completion/index.js
+++ src/components/Completion/index.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { ApolloProvider } from "react-apollo";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
-import client from 'client';
-import AuthContext from 'components/context/Auth';
-import SelectedDateContext from 'components/context/SelectedDate';
-import LoadingAndErrorHandler from 'components/LoadingAndErrorHandler';
-import AuthModel from 'models/Auth';
-import GoalModel, { goalCompletionForDate } from 'models/Goal';
+import client from "client";
+import AuthContext from "components/context/Auth";
+import SelectedDateContext from "components/context/SelectedDate";
+import LoadingAndErrorHandler from "components/LoadingAndErrorHandler";
+import AuthModel from "models/Auth";
+import GoalModel, { goalCompletionForDate } from "models/Goal";
+import styles from "./styles.module.css";
 
 const CREATE_COMPLETION_MUTATION = gql`
   mutation CreateCompletion(
-    $auth: AuthInput!,
-    $goalId: Int!,
+    $auth: AuthInput!
+    $goalId: Int!
     $completedAt: ISO8601DateTime!
-  ){
+  ) {
     createCompletion(auth: $auth, goalId: $goalId, completedAt: $completedAt) {
       goals {
         id
@@ -29,7 +30,7 @@ const CREATE_COMPLETION_MUTATION = gql`
 `;
 
 const DELETE_COMPLETION_MUTATION = gql`
-  mutation DeleteCompletion($auth: AuthInput!, $completionId: Int!){
+  mutation DeleteCompletion($auth: AuthInput!, $completionId: Int!) {
     deleteCompletion(auth: $auth, completionId: $completionId) {
       goals {
         id
@@ -42,14 +43,17 @@ const DELETE_COMPLETION_MUTATION = gql`
 `;
 
 class Completion extends Component {
-  completion = () => goalCompletionForDate({
-    goal: this.props.goal, 
-    dateString: this.props.selectedDate
-  })
+  completion = () =>
+    goalCompletionForDate({
+      goal: this.props.goal,
+      dateString: this.props.selectedDate
+    });
 
-  checked = () => !!this.completion()
+  checked = () => !!this.completion();
 
-  toggle = () => {
+  toggle = event => {
+    event.preventDefault();
+
     if (!this.checked()) {
       return this.props.createCompletion({
         variables: {
@@ -66,20 +70,63 @@ class Completion extends Component {
         completionId: this.completion().id
       }
     });
-  }
+  };
 
   render() {
+    const tickCoverClass = `
+      ${styles.box}
+      ${styles.tickCover}
+      ${this.checked() && styles.tickCoverChecked}
+    `;
+
     return (
       <LoadingAndErrorHandler
-        loading={this.props.loading}
+        loading={false}
         error={this.props.error}
       >
-        <input
-          name="completed"
-          type="checkbox"
-          checked={this.checked()}
-          onChange={this.toggle}
-        />
+        <div className={styles.tickBox} onClick={this.toggle}>
+          <input name="completed" type="checkbox" onChange={this.toggle} />
+
+          <svg
+            width="630px"
+            height="630px"
+            viewBox="0 0 630 630"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g
+              stroke="none"
+              stroke-width="1"
+              fill="none"
+              fill-rule="evenodd"
+            >
+              <rect
+                className={styles.box}
+                fill="#8D5687"
+                x="0"
+                y="0"
+                width="630"
+                height="630"
+                rx="8"
+              />
+              <polyline
+                className={styles.tick}
+                stroke="none"
+                stroke-width="100"
+                points="109 293.646424 229.186565 440 529 152"
+              />
+              <rect
+                className={tickCoverClass}
+                fill="#8D5687"
+                x="0"
+                y="0"
+                width="630"
+                height="630"
+                rx="8"
+              />
+            </g>
+          </svg>
+        </div>
       </LoadingAndErrorHandler>
     );
   }
@@ -92,7 +139,7 @@ Completion.propTypes = {
   createCompletion: PropTypes.func.isRequired,
   goal: GoalModel.isRequired,
   selectedDate: PropTypes.string.isRequired
-}
+};
 
 export { Completion };
 
@@ -104,13 +151,9 @@ class CompletionWrapped extends Component {
           <AuthContext.Consumer>
             {auth => (
               <ApolloProvider client={client}>
-                <Mutation
-                  mutation={CREATE_COMPLETION_MUTATION}
-                >
+                <Mutation mutation={CREATE_COMPLETION_MUTATION}>
                   {(createCompletion, create) => (
-                    <Mutation
-                      mutation={DELETE_COMPLETION_MUTATION}
-                    >
+                    <Mutation mutation={DELETE_COMPLETION_MUTATION}>
                       {(deleteCompletion, remove) => (
                         <Completion
                           loading={create.loading || remove.loading}
@@ -129,13 +172,13 @@ class CompletionWrapped extends Component {
             )}
           </AuthContext.Consumer>
         )}
-    </SelectedDateContext.Consumer>
+      </SelectedDateContext.Consumer>
     );
   }
 }
 
 CompletionWrapped.propTypes = {
   goal: GoalModel
-}
+};
 
 export default CompletionWrapped;
